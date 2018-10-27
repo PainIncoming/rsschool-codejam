9650b9c5fcee6e9e4e0398803b0c145a5bafa3e6
PainIncoming <galko3.v@gmail.com>
2018-10-28 01:52:39 +0300

    refactor: refactoring of the make and sumOfOther functions

--- src/make.js
+++ src/make.js
@@ -1,13 +1,16 @@
 module.exports = function make(...init) {
   const numbers = [];
-  const next = (...args) => {
+
+  return (function next(...args) {
     if (typeof args[0] !== 'function') {
       args.forEach(item => numbers.push(item));
       return next;
     }
 
-    return numbers.reduce(args[0], 0);
-  };
+    if (numbers.length) {
+      return numbers.reduce(args[0]);
+    }
 
-  return next(...init);
+    throw new Error('No items');
+  }(...init));
 };
--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,4 +1,6 @@
 module.exports = function sumOfOther(arr) {
+  if (!arr.length) throw new Error('Array is empty');
+
   const sumOfAll = arr.reduce((sum, curr) => sum + curr);
 
   return arr.map(item => sumOfAll - item);
