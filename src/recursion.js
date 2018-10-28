a62e3002f6c7dd1939917455553e6dad1977784c
PainIncoming <galko3.v@gmail.com>
2018-10-28 05:27:42 +0300

    feat: full implementation of the recursion function

--- src/recursion.js
+++ src/recursion.js
@@ -1,5 +1,19 @@
 module.exports = function recursion(tree) {
-  if (!Object.keys(tree).length) throw new Error('Invalid tree');
+  if (!tree) throw new Error('Need more tree!');
 
-  return [];
+  return (function getChilds(...args) {
+    const result = [];
+    const nextArgs = [];
+
+    args.forEach((item) => {
+      result.push(item.value);
+
+      if (item.left) nextArgs.push(item.left);
+      if (item.right) nextArgs.push(item.right);
+    });
+
+    if (nextArgs.length) return [result].concat(getChilds(...nextArgs));
+
+    return [result];
+  }(tree));
 };
